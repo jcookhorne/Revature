d364564ea023beb3f20ae6772483547a074a6641
Joshua Cookhorne <joshua.cookhorne@revature.net>
2022-03-02 13:39:57 -0500

    Update dbscript.sql

--- Project_1/src/dbscript.sql
+++ Project_1/src/dbscript.sql
@@ -3,5 +3,8 @@ CREATE DATABASE project_1;
 CREATE TABLE employee_details(employee_id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY, first_name varchar(25), last_name varchar(25), address varchar(25), contact varchar(12), email varchar(25), username varchar(25), password varchar(25));
 
 
+CREATE TABLE manager_details(manager_id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY, first_name varchar(25), last_name varchar(25), address varchar(25), contact varchar(12), email varchar(25), username varchar(25), password varchar(25));
 
+
+CREATE TABLE reimbursement_details(reimbursment_id int GENERATED ALWAYS AS IDENTITY, employee_id int, reimbursement_amount float(2), reimbursement_reason varchar(250), reimbursement_date datetime DEFAULT GETDATE(), PRIMARY KEY (reimbursment_id), FOREIGN KEY(employee_id) REFERENCES employee_details(employee_id));
 
