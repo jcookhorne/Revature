661cb7b098b90e630e2994b025559999d49c2138
Joshua Cookhorne <joshua.cookhorne@revature.net>
2022-03-02 13:03:15 -0500

    Create dbscript.sql

---
+++ Project_1/src/dbscript.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE project_1;
+
+CREATE TABLE employee_details(employee_id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY, first_name varchar(25), last_name varchar(25), address varchar(25), contact varchar(12), email varchar(25), username varchar(25), password varchar(25));
+
+
+
+
